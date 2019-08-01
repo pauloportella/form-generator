85dae36cd192cb781df80ca69524325e4ced1c0b
Paulo Portella <paulo.portella@radicalcompany.com>
2019-08-01 18:22:45 +0100

    added FormFields componnent

--- src/components/FormFields/FormFields.tsx
+++ src/components/FormFields/FormFields.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { FormField } from 'components';
-import { FormikProps } from 'formik';
+import { getField } from 'services/form';
 
 export interface IFormFieldsProps {
   fields: IField[];
-  formikProps: any ;
+  formikProps: any;
   isUpdate: boolean;
 }
 
@@ -15,45 +15,41 @@ export default function FormFields({
 }: IFormFieldsProps): React.ReactElement {
   const { values, handleChange, handleBlur } = formikProps;
 
-    return (<>
-        
-
-        {fields.map(({
-             id,
-             label,
-             fieldType = FieldType.TEXT,
-             options,
-             multiline = false,
-             type,
-             inputProps,
-            disableOnUpdate = false,
-             
+  return (
+    <>
+      {fields.map(
+        ({
+          id,
+          label,
+          fieldType = FieldType.TEXT,
+          options,
+          multiline = false,
+          type,
+          inputProps,
+          disableOnUpdate = false,
         }) => {
-            const isSelect = Boolean(options && options.length)
+          const isSelect = Boolean(options && options.length);
 
-
-            return (
-                <FormField
-                key={id}
-                id={id}
-                label={label}
-                select={isSelect}
-                multiline={multiline}
-                value={values[id]}
-                options={options}
-                component={getField(fieldType)}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                type={type}
-                inputProps={inputProps}
-                disabled={isUpdate && disableOnUpdate}
-            )
-        })}
-    
-    
-    
-    
-    
-    
-    </>)
+          return (
+            <FormField
+              key={id}
+              id={id}
+              label={label}
+              select={isSelect}
+              multiline={multiline}
+              value={values[id]}
+              options={options}
+              component={getField(fieldType)}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              type={type}
+              inputProps={inputProps}
+              disabled={isUpdate && disableOnUpdate}
+            />
+          );
+        }
+      )}
+      }
+    </>
+  );
 }
