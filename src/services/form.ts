9ef714c6451049afc068b60b209d044af972230f
Paulo Portella <paulo.portella@radicalcompany.com>
2019-08-01 13:21:42 +0100

    added global types

---
+++ src/services/form.ts
@@ -0,0 +1,2 @@
+import React from 'react';
+import * as Yup from 'yup';
---
+++ src/types/global.d.ts
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as Yup from 'yup';
+
+declare enum FieldType {
+  TEXT = 'TEXT',
+  NUMERIC = 'NUMERIC',
+  CHECKBOX = 'CHECKBOX',
+  SELECT = 'SELECT',
+  MULTI_SELECT = 'MULTI_SELECT',
+  SEARCH_SELECT = 'SEARCH_SELECT',
+  RADIO_BUTTON = 'RADIO_BUTTON',
+  RADIO_INPUT = 'RADIO_INPUT',
+  TEXT_AREA = 'TEXT_AREA',
+  DATE_PICKER = ' DATE_PICKER',
+}
+
+interface IOptions {
+  label: string;
+  value: string;
+}
+
+interface IField {
+  id: string;
+  label: string;
+  multiline?: boolean;
+  options?: IOptions[];
+  initialValue?: string | number | {};
+  fieldType?: FieldType;
+  disableOnUpdate?: boolean;
+  validation:
+    | Yup.MixedSchema
+    | Yup.StringSchema
+    | Yup.NumberSchema
+    | Yup.BooleanSchema
+    | Yup.DateSchema;
+  type?: RadioTypes | string;
+  inputProps?: any;
+}
