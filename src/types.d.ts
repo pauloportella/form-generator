ff1fc1a91eb65e5b2800026615da479057dc784d
Paulo Portella <paulo.portella@radicalcompany.com>
2019-08-01 14:40:01 +0100

    setup types

---
+++ src/types.d.ts
@@ -0,0 +1,46 @@
+import React from 'react';
+import * as Yup from 'yup';
+
+declare global {
+  export enum FieldType {
+    TEXT = 'TEXT',
+    NUMERIC = 'NUMERIC',
+    CHECKBOX = 'CHECKBOX',
+    SELECT = 'SELECT',
+    MULTI_SELECT = 'MULTI_SELECT',
+    SEARCH_SELECT = 'SEARCH_SELECT',
+    RADIO_BUTTON = 'RADIO_BUTTON',
+    RADIO_INPUT = 'RADIO_INPUT',
+    TEXT_AREA = 'TEXT_AREA',
+    DATE_PICKER = ' DATE_PICKER',
+  }
+
+  export enum ValidationRules {
+    DEFAULT = 'DEFAULT',
+    DATE = 'DATE',
+    CURRENCY = 'CURRENCY',
+  }
+
+  interface IOptions {
+    label: string;
+    value: string;
+  }
+
+  export interface IField {
+    id: string;
+    label: string;
+    multiline?: boolean;
+    options?: IOptions[];
+    initialValue?: string | number | {};
+    fieldType?: FieldType;
+    disableOnUpdate?: boolean;
+    validation:
+      | Yup.MixedSchema
+      | Yup.StringSchema
+      | Yup.NumberSchema
+      | Yup.BooleanSchema
+      | Yup.DateSchema;
+    type?: string;
+    inputProps?: any;
+  }
+}
